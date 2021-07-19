b843fd0e4177682c4c9c05cffffedb1cb2215321
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2021-07-19 18:13:48 -0300

    Update miles logic

--- frontend/script.js
+++ frontend/script.js
@@ -1,65 +1,57 @@
 //O que falta fazer: 
-// 1. Fazer com que as informações se alterem quando o valor é alterado
-// 2. Remover cidade igual quando viagem para mesmo país
 // 3. Pegar latutude e longitude de cidade de origem e destino
-// 4. Fazer milhas funcionar
+
+const numberFormatter = new Intl.NumberFormat('pt-BR');
+const moneyFormatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
 
 let countryOrigin = document.querySelector('#pais-origem');
 let countryDestination = document.querySelector('#pais-destino');
 let cityOrigin = document.querySelector('#cidade-origem');
 let cityDestination = document.querySelector('#cidade-destino');
 let distance = 0;
-let latOrigin = null;
-let lonOrigin = null;
-let latDestination = null;
-let lonDestination = null;
+let latOrigin = 25;
+let lonOrigin = 15;
+let latDestination = -95;
+let lonDestination = 12;
 let radioEconomicClass = document.querySelector('#classe-economica');
 let radioFirstClass = document.querySelector('#classe-executiva');
 let adults = document.querySelector('#num-adultos');
-let childrem = document.querySelector('#num-criancas');
+let children = document.querySelector('#num-criancas');
 let costTicketAdult = 0;
 let costTicketChild = 0;
 let totalCost = 0;
 let results = document.querySelector('#resultado');
 let countTotalTrips = document.querySelector('#countTotalTrips');
-let maxMiles = 0;
+let miles = 0;
+let milesCost = 0;
 
-function increaseAdult() {
-    parseInt(adults.value, 10);
-    adults = isNaN(adults) ? 0 : adults;
-    adults++;
-    document.querySelector('#num-adultos').value = adults;
+function helperFormatValue(value) {
+    return numberFormatter.format(value);
+}
+  
+function helperFormatMoney(value) {
+    return moneyFormatter.format(value);
 }
 
-function decreaseAdult() {
-    parseInt(adults.value, 10);
-    adults = isNaN(adults) ? 0 : adults;
-    adults < 2 ? adults = 2 : '';
-    adults--;
-    document.querySelector('#num-adultos').value = adults;
-}
-
-function increaseChild() {
-    parseInt(childrem.value, 10);
-    childrem = isNaN(childrem) ? 0 : childrem;
-    childrem++;
-    document.querySelector('#num-criancas').value = childrem;
-}
-
-function decreaseChild() {
-    parseInt(childrem.value, 10);
-    childrem = isNaN(childrem) ? 0 : childrem;
-    childrem < 1 ? childrem = 1 : '';
-    childrem--;
-    document.querySelector('#num-criancas').value = childrem;
-}
+function degreesToRadians(degreeValue) {
+    return degreeValue * (Math.PI / 180);
+} 
 
 function updateMilesInput(val) {
-    document.querySelector('#milesValue').value=val; 
-  }
+    miles = val; 
+    renderResults();
+}
+
+function ticketWithMiles() {
+    milesCost = miles * 0.02;
+    totalCost = totalCost - milesCost;
+    milesCost = helperFormatMoney(milesCost)
+}
 
 async function start() {
-
     await fetchCountries()
 }
 
@@ -75,6 +67,7 @@ function render() {
 
     renderCountryOption();
     renderCityOption();
+    renderLatAndLon();
     renderDistance();
     renderPrice();
     renderResults();
@@ -122,90 +115,83 @@ function renderCityOption() {
     cityOrigin.innerHTML = cityOriginHTML;
     cityDestination.innerHTML = cityDestinationHTML;
 
-    //remover a cidade igual de origem e destino
-    if (cityOrigin.value === cityDestination.value) {
-        cityDestination.remove(cityDestination.value)
-    }
+    renderResults();
+}
+
+function renderLatAndLon() {
+
 }
 
 function renderDistance() {
-    // pegar latutude e longitude de cidade de origem e destino
-    // console.log(allTrips[0].cities[0].latitude)
+    const EARTH_RADIUS = 6_371.071; // Earth
 
-    for (let i = 0; i < allTrips[i].length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            if(allTrips[i].cities[j] === cityOrigin.value) {
-                latOrigin = allTrips[i].cities[j].latitude
-                lonOrigin = allTrips[i].cities[j].longitude
-                console.log('latOrigin' + latOrigin + ', lonOrigin' + lonOrigin)
-            }
-        }
-    }
-
-    // teste
-    latDestination = -23.54;
-    latOrigin = -29.36;
-    lonDestination = -50.87;
-    lonOrigin = -34.88;
-
-    //cálculo de distância
-
-    var R = 6371.0710; // Radial da terra em quilômetros
-    var difflat = latDestination-latOrigin; // Diferença em radial (latitudes)
-    var difflon = lonDestination-lonOrigin; // Diferença em radial (longitudes)
-
-    distance = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(latOrigin)*Math.cos(latDestination)*Math.sin(difflon/2)*Math.sin(difflon/2)));
-    distance = distance.toFixed(2)
+    const diffLatitudeRadians = degreesToRadians(
+      latDestination - latOrigin
+    );
+  
+    const diffLongitudeRadians = degreesToRadians(
+      lonDestination - lonOrigin
+    );
+  
+    const originLatitudeRadians = degreesToRadians(latDestination);
+    const destinationLatitudeRadians = degreesToRadians(lonDestination);
+  
+    const kmDistance =
+      2 *
+      EARTH_RADIUS *
+      Math.asin(
+        Math.sqrt(
+          Math.sin(diffLatitudeRadians / 2) * Math.sin(diffLatitudeRadians / 2) +
+            Math.cos(originLatitudeRadians) *
+              Math.cos(destinationLatitudeRadians) *
+              Math.sin(diffLongitudeRadians / 2) *
+              Math.sin(diffLongitudeRadians / 2)
+        )
+      );
+  
+    distance = kmDistance.toFixed(2)
 }
 
 function renderPrice() {    
-    let totalCostForAdults = 0;
-    let totalCostForChildrem = 0;
-    // console.log(`distance: ${distance}, countryOrigin: ${countryOrigin.value}, countryDestination: ${countryDestination.value}`)
-
-    if (countryOrigin.value === countryDestination.value) {
-        costTicketAdult = parseInt(distance * 0.3).toFixed(2);
-        costTicketChild = parseInt(distance * 0.15).toFixed(2);
-    } else {
-        costTicketAdult = parseInt(distance * 0.5).toFixed(2);
-        costTicketChild = parseInt(distance * 0.25).toFixed(2);
-    }
-    // console.log(`costTicketAdult: ${costTicketAdult}, costTicketChild: ${costTicketChild}`)
+    countryOrigin.value === countryDestination.value ? costTicketAdult = distance * 0.3 : costTicketAdult = distance * 0.5;
+    countryOrigin.value === countryDestination.value ? costTicketChild = distance * 0.15 : costTicketChild = distance * 0.25;
 
     if (radioFirstClass.checked) {
-        costTicketAdult = costTicketAdult * 1.8;
-        costTicketChild = costTicketChild * 1.4;
+        costTicketAdult = helperFormatMoney(costTicketAdult * 1.8);
+        costTicketChild = helperFormatMoney(costTicketChild * 1.4);
     }
 
-    totalCostForAdults = costTicketAdult * parseInt(adults.value);
-    totalCostForChildrem = costTicketChild * parseInt(childrem.value);
+    totalCost = (costTicketAdult * adults.value) + (costTicketChild * children.value);
+    
+    miles !== 0 && ticketWithMiles();
 
-    totalCost = totalCostForAdults + totalCostForChildrem
-    totalCost = totalCost.toFixed(2)
-    // console.log(`totalCost ${totalCost}`)
+    costTicketAdult = helperFormatMoney(costTicketAdult)
+    costTicketChild = helperFormatMoney(costTicketChild)
+    totalCost = helperFormatMoney(totalCost)
 };
 
 function renderResults() {
-    // fazer milhas funcionar
-    results.innerHTML = `
-    <h2>Resumo da viagem</h2>
-    <p>Origem: ${countryOrigin.value}</p>
-    <p>Destino: ${countryDestination.value} </p>
-    <p>Distância: ${distance}km</p>
-    <p>${adults.value} adultos, ${childrem.value} crianças</span></p>
-    <p>Tipo de voo: Classe ${radioFirstClass.checked ? 'Executiva' : 'Econômica'}</p>
-    <p>R$ ${costTicketAdult} por adulto</p>
-    <p>R$ ${costTicketChild} por criança</p>
-    <p>Valor total das passagens: R$ ${totalCost}</p>
-    `
-}
-
-function ticketWithMiles(miles, totalCost) {
-    return console.log(totalCost - (miles * 0.02));
-}
-
-function calcMaxMiles(totalCost) {
-    maxMiles = totalCost / 0.02;
+    renderPrice()
+    //remover a cidade igual de origem e destino
+    if (cityOrigin.value === cityDestination.value) {
+        results.innerHTML = `
+        <p>Não é possível viajar para a mesma cidade de origem</p>
+        `
+    } else {
+        results.innerHTML = `
+        <h2>Resumo da viagem</h2>
+        <p>Origem: ${countryOrigin.value}</p>
+        <p>Destino: ${countryDestination.value} </p>
+        <p>Distância: ${distance}km</p>
+        <p>${adults.value} adultos, ${children.value} crianças</span></p>
+        <p>Tipo de voo: Classe ${radioFirstClass.checked ? 'Executiva' : 'Econômica'}</p>
+        <p>${costTicketAdult} por adulto</p>
+        <p>${costTicketChild} por criança</p>
+        <p>Valor total das passagens: ${totalCost}</p>
+        <p>Quantidade de milhas usadas: ${miles} </p>
+        <p>Valor economizado por milhas: ${milesCost}</p>
+        `
+    }
 }
 
 start();
